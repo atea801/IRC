3059759753dda909998fefc0b22d9bc1ef6090f9
bkaras-g <bkaras-g@student.42.fr>
2026-06-16 15:06:55 +0200

    doc [Channel]: ajout precision que les OCF sont a implementer

--- src/Channel/Channel.cpp
+++ src/Channel/Channel.cpp
@@ -6,29 +6,33 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 15:02:38 by bkaras-g          #+#    #+#             */
-/*   Updated: 2026/06/16 15:02:58 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2026/06/16 15:06:12 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Channel.hpp"
 #include <iostream>
 
+//a implementer en prenant en compte les variables membres
 Channel::Channel(void)
 {
     std::cout << "Channel default constructor called" << std::endl;
 }
 
+//a implementer en prenant en compte les variables membres
 Channel::Channel(const Channel& copy)
 {
     std::cout << "Channel copy constructor called" << std::endl;
     *this = copy;
 }
 
+//a implementer en prenant en compte les variables membres
 Channel::~Channel(void)
 {
     std::cout << "Channel destructor called" << std::endl;
 }
 
+//a implementer en prenant en compte les variables membres
 Channel& Channel::operator=(const Channel& copy)
 {
     std::cout << "Channel assignment operator called" << std::endl;
