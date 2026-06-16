775d5061f58315be1a0e4eb886c3afe7bb8ef742
aautret <aautret@student.42.fr>
2026-06-16 16:38:28 +0200

    feat[message] : mise en place de focntion pour les parsers
    - extraction d'une commande depuis le buffer de client
    - preparation des foncitons membres pour les parsers specifiques aux commandes et arguments des commandes

--- includes/message.hpp
+++ includes/message.hpp
@@ -1,14 +1,20 @@
 #pragma once
 
+#include "Client.hpp"
 #include <string>
 #include <vector>
 
-class message {
-    private :
-        std::string command;
-        std::vector<std::string> args;
-    public :
-    int message_objects(std::string data);
+class message
+{
+  private:
+    std::string command;
+    std::vector<std::string> args;
+
+  public:
+    void extract_and_clean(Client &c);
+    int parser_cmd(std::string cmd);
+    int parser_arg(std::vector<std::string> args);
+    void fill_cmd_and_args(size_t pos, std::string data);
     const std::string &get_command() const;
     const std::vector<std::string> &get_args() const;
 };
--- src/message/message_parser.cpp
+++ src/message/message_parser.cpp
@@ -1,56 +1,59 @@
 #include "message.hpp"
 #include "server.hpp"
 
+void message::fill_cmd_and_args(size_t pos, std::string data)
+{
+    int j = 0;
+    int flag_com = 0;
+    for (size_t i = 0; i < pos; i++)
+    {
+        while (data[i] == ' ')
+            i++;
+        if (data[i] == ' ' && flag_com == 0)
+        {
+            flag_com = 1;
+            this->command = data.substr(0, i);
+            j = i + 1;
+        }
+        else if (data[i] == ' ' && flag_com == 1)
+        {
+            std::string arg = data.substr(j, i - j);
+            this->args.push_back(arg);
+            j = i + 1;
+        }
+        else if (data[i] == ':')
+        {
+            std::string arg = data.substr(i + 1, pos - (i + 1));
+            this->args.push_back(arg);
+        }
+        else if (i == pos - 1)
+        {
+            std::string arg = data.substr(j, i - j + 1);
+            this->args.push_back(arg);
+        }
+    }
+}
+
 /**
  * @brief une fonction qui vient remplir les attribut priver de la structure
  * cmd_args (command et argument) en ce referent a ce qu entre l utilisateur
  * dans sa boite de commande
- * 
+ *
  * @param data (buffer remplie par recv)
- * @return int 
+ * @return int
  */
-int
-message::message_objects (std::string data)
+void message::extract_and_clean(Client &c)
 {
-  size_t pos = data.find ("\r\n");
-  bool is_space = (data.find (' ') != std::string::npos);
-  if (pos == std::string::npos)
-    return (-1);
-  int flag_com = 0;
-  int j = 0;
-  if (is_space == true)
-    {
-      for (int i = 0; i < pos; i++)
-        {
-          while (data[i] == ' ')
-            i++;
-          if (data[i] == ' ' && flag_com == 0)
-            {
-              flag_com = 1;
-              this->command = data.substr (0, i);
-              j = i + 1;
-            }
-          else if (data[i] == ' ' && flag_com == 1)
-            {
-              std::string arg = data.substr (j, i - j);
-              this->args.push_back (arg);
-              j = i + 1;
-            }
-          else if (data[i] == ':')
-            {
-              std::string arg = data.substr (i + 1, pos - (i + 1));
-              this->args.push_back (arg);
-              return (0);
-            }
-          else if (i == pos - 1)
-            {
-              std::string arg = data.substr (j, i - j + 1);
-              this->args.push_back (arg);
-              return (0);
-            }
-        }
-    }
-  else
-    this->command = data.substr (0, pos);
-  return (0);
+    // 1. extraire la premiere commande
+    std::string data = c.getBuffer();
+    size_t pos = data.find("\r\n");
+    bool is_space = (data.find(' ') != std::string::npos);
+    int flag_com = 0;
+    int j = 0;
+    if (is_space == true)
+        fill_cmd_and_args(pos, data);
+    else
+        this->command = data.substr(0, pos);
+    // 2. nettoie le buf
+    c.setBuffer(data.substr(pos + 2));
 }
