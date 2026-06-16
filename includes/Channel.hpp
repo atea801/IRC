c30ca0729a835df8405de50ba75726b70fadbb31
bkaras-g <bkaras-g@student.42.fr>
2026-06-16 15:59:08 +0200

    feat [Channel]: ajout des getters + setters et maj des variables membres suite relecture de Claude

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 14:57:26 by bkaras-g          #+#    #+#             */
-/*   Updated: 2026/06/16 15:49:03 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2026/06/16 15:49:43 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,16 @@
 class Channel {
 private:
     // ---------- Private Data members -------------------------
+    std::string             _name;
     std::vector<Client *>   _members;
     std::vector<Client *>   _chanOps;
+    std::vector<Client *>   _invited;
     std::string             _topic;
-    bool                    _is_invite_only;
-    bool                    _has_a_password;
+    bool                    _topic_restricted;  // mode +t : only ops can set topic
+    bool                    _is_invite_only;    // mode +i
+    bool                    _has_a_password;    // mode +k
     std::string             _password;
-    bool                    _has_a_user_limit;
+    bool                    _has_a_user_limit;  // mode +l
     size_t                  _user_limit;
 
 public:
@@ -42,7 +45,29 @@ public:
 
     // ---------- Getter and Setter Methods ------------
 
+    const std::string&              getName() const;
+    const std::vector<Client *>&    getMembers() const;
+    const std::vector<Client *>&    getChanOps() const;
+    const std::vector<Client *>&    getInvited() const;
+    const std::string&              getTopic() const;
+    bool                            isTopicRestricted() const;
+    bool                            isInviteOnly() const;
+    bool                            hasPassword() const;
+    const std::string&              getPassword() const;
+    bool                            hasUserLimit() const;
+    size_t                          getUserLimit() const;
+
+    void    setName(const std::string& name);
+    void    setTopic(const std::string& topic);
+    void    setTopicRestricted(bool restricted);
+    void    setInviteOnly(bool inviteOnly);
+    void    setPassword(const std::string& password);
+    void    removePassword();
+    void    setUserLimit(size_t limit);
+    void    removeUserLimit();
+
     // ---------- Member Methods -----------------------
+    // addMember / removeMember / addOperator / isMember / invite ...
 
 };
 
