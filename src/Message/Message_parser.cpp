72ee410403d7c9b58f46a3b27acc86cd62cf6cf7
TOM <tomlorette2703@gmail.com>
2026-06-17 13:52:58 +0200

    feature[message] parsing de commandes et args

--- src/Message/Message_parser.cpp
+++ src/Message/Message_parser.cpp
@@ -1,6 +1,11 @@
 #include "Message.hpp"
 
-
+/**
+ * @brief remplie les attributs priver commande et args de la class message
+ *
+ * @param pos
+ * @param data
+ */
 void Message::fill_cmd_and_args(size_t pos, std::string data)
 {
     int j = 0;
@@ -24,6 +29,7 @@ void Message::fill_cmd_and_args(size_t pos, std::string data)
         else if (data[i] == ':')
         {
             std::string arg = data.substr(i + 1, pos - (i + 1));
+            this->trailing_arg = true;
             this->args.push_back(arg);
         }
         else if (i == pos - 1)
@@ -35,9 +41,9 @@ void Message::fill_cmd_and_args(size_t pos, std::string data)
 }
 
 /**
- * @brief une fonction qui vient remplir les attribut priver de la structure
- * cmd_args (command et argument) en ce referent a ce qu entre l utilisateur
- * dans sa boite de commande
+ * @brief extraction d elements commande et args a partir
+ * du buffer d un client precis et suppression des elements
+ * apres recuperation. permet de garder le buffer client a jour.
  *
  * @param data (buffer remplie par recv)
  * @return int
@@ -55,3 +61,36 @@ void Message::extract_and_clean(Client &c)
     // 2. nettoie le buf
     c.setBuffer(data.substr(pos + 2));
 }
+
+/**
+ * @brief parsing de l attribut prive command de la class message
+ *
+ * @param cmd
+ * @return int
+ */
+int Message::parser_cmd(std::string cmd)
+{
+    if (cmd == "NICK" || cmd == "QUIT" || cmd == "JOIN" || cmd == "PASS" || cmd == "USER" || cmd == "PART" ||
+        cmd == "TOPIC" || cmd == "MODE" || cmd == "INVITE" || cmd == "KICK" || cmd == "PRIVMSG" || cmd == "PING" ||
+        cmd == "PONG" || cmd == "CAP")
+        return (0);
+    return (-1);
+}
+
+int Message::handle_quit(std::vector<std::string> args)
+{
+    if (args.size() == 0)
+        return (0);
+    else if (args.size() == 1 && this->trailing_arg == true)
+    {
+        std::string arg = args[0];
+        for (int i = 0; i < arg.size(); i++)
+        {
+            
+        }
+        return (1);
+    }
+    else
+        return (-1);
+}
+
