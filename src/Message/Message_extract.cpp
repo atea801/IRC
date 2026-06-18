a8790eb44dbfc82a2423841afd14a16cedde1187
TOM <tomlorette2703@gmail.com>
2026-06-18 15:39:34 +0200

    fix[server] mettre command en uppercase
    mise de command systematiquement en upercase pour evite les probleme si on envoie des chose comme nIcK qui sont valide dans rfc

--- src/Message/Message_extract.cpp
+++ src/Message/Message_extract.cpp
@@ -10,18 +10,23 @@
 void Message::fill_cmd_and_args(size_t pos, std::string data)
 {
     size_t i = 0;
-    
+    //gestion prefix.
     // 1. extraire la commande jusqu'au premier espace
     size_t space = data.find(' ');
     if (space == std::string::npos || space >= pos)
     {
         // pas d'espace → que la commande, pas d'args
+        for (size_t i = 0; i < data.size(); ++i)
+            data[i] = std::toupper(data[i]);
         this->command = data.substr(0, pos);
         return ;
     }
+    // 1. mettre command en maj puis assigner le premier element 
+    //avant le premier espace a command
     this->command = data.substr(0, space);
+    for (size_t i = 0; i < this->command.size(); ++i)
+            this->command[i] = std::toupper(this->command[i]);
     i = space + 1;
-
     // 2. extraire les args
     while (i < pos)
     {
@@ -63,11 +68,7 @@ void Message::extract_and_clean(Client &c)
     // 1. extraire la premiere commande
     std::string data = c.getBuffer();
     size_t pos = data.find("\r\n");
-    bool is_space = (data.find(' ') != std::string::npos);
-    if (is_space == true)
-        fill_cmd_and_args(pos, data);
-    else
-        this->command = data.substr(0, pos);
+    fill_cmd_and_args(pos, data);
     // 2. nettoie le buf
     c.setBuffer(data.substr(pos + 2));
 }
