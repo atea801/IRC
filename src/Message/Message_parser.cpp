88b301bfc51fb1050277fa3a095776fa0f721acd
aautret <aautret@student.42.fr>
2026-06-18 17:22:49 +0200

    fix[error]: chagement des message enum conforme doc RFC

--- src/Message/Message_parser.cpp
+++ src/Message/Message_parser.cpp
@@ -4,20 +4,20 @@ IrcError Message::parsing_nick()
 {
     // 1 arg est vide
     if (this->args.empty())
-        return ERR_EMPTY;
+        return ERR_NONICKNAMEGIVEN;
     // 2 check qu'il y a bien qu'un seul str dans le vecteur
     if (this->args.size() > 1 || args[0].size() > 9)
         return ERR_INVALID;
     // 3 check premier caractere
     std::string special = "-_[]{}\\|";
     if (!isalpha(args[0][0]) && special.find(this->args[0][0]) == std::string::npos)
-        return ERR_INVALID;
+        return ERR_ERRONEUSNICKNAME;
     // 3 check caracteres
     for (size_t i = 1; i < this->args[0].size(); i++)
     {
         // si le caractere n'est pas une lettre/chiffre et qu'il n'est pas dans les car speciaux
         if (!isalnum(args[0][i]) && special.find(this->args[0][i]) == std::string::npos)
-            return ERR_INVALID;
+        return ERR_ERRONEUSNICKNAME;
     }
     return IRC_OK;
 }
