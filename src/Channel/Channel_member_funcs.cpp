9dafa7736113faf2b2053011f5f41cfc8f1a4dcc
bkaras-g <bkaras-g@student.42.fr>
2026-06-19 12:19:01 +0200

    feat [Channel]: ajout de isMember()

--- src/Channel/Channel_member_funcs.cpp
+++ src/Channel/Channel_member_funcs.cpp
@@ -6,7 +6,7 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/18 16:17:53 by bkaras-g          #+#    #+#             */
-/*   Updated: 2026/06/19 12:08:06 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2026/06/19 12:18:33 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,17 @@ void    Channel::removeMember(Client &c)
     it = std::find(this->getMembers().begin(), this->getMembers().end(), &c);
     this->_members.erase(it);
 }
-//std::find
-//swap et pop_back
+
+/*
+Vérifie si le client `c` est membre du channel.
+*/
+bool    Channel::isMember(Client &c)
+{
+    std::vector<Client *>::const_iterator it;
+    it = std::find(this->getMembers().begin(), this->getMembers().end(), &c);
+    if (it == this->getMembers().end())
+        return (false);
+    else
+        return (true);
+}
 
